0db18c2efaa60259291425a27a556a47c115d2d6
Didier WECKMANN <didier.weckmann@ircad.fr>
2019-11-20 17:09:30 +0100

    feat(conanfile): update to stable

--- conanfile.py
+++ conanfile.py
@@ -30,20 +30,20 @@ class LibVTKConan(ConanFile):
             os.environ["CONAN_SYSREQUIRES_MODE"] = "verify"
 
     def requirements(self):
-        self.requires("common/1.0.2@sight/testing")
-        self.requires("qt/5.12.4-r2@sight/testing")
+        self.requires("common/1.0.2@sight/stable")
+        self.requires("qt/5.12.4-r2@sight/stable")
 
         if tools.os_info.is_windows:
-            self.requires("libxml2/2.9.8-r4@sight/testing")
-            self.requires("expat/2.2.5-r4@sight/testing")
-            self.requires("zlib/1.2.11-r4@sight/testing")
+            self.requires("libxml2/2.9.8-r4@sight/stable")
+            self.requires("expat/2.2.5-r4@sight/stable")
+            self.requires("zlib/1.2.11-r4@sight/stable")
 
         if not tools.os_info.is_linux:
-            self.requires("glew/2.0.0-r4@sight/testing")
-            self.requires("libjpeg/9c-r4@sight/testing")
-            self.requires("freetype/2.9.1-r4@sight/testing")
-            self.requires("libpng/1.6.34-r4@sight/testing")
-            self.requires("libtiff/4.0.9-r4@sight/testing")
+            self.requires("glew/2.0.0-r4@sight/stable")
+            self.requires("libjpeg/9c-r4@sight/stable")
+            self.requires("freetype/2.9.1-r4@sight/stable")
+            self.requires("libpng/1.6.34-r4@sight/stable")
+            self.requires("libtiff/4.0.9-r4@sight/stable")
 
     def build_requirements(self):
         if tools.os_info.linux_distro == "linuxmint":
